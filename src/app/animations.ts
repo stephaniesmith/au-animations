0c275421119b96d743f047e0e4dae362ef01b13d
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-05-06 08:37:35 -0700

    animations working

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,10 +1,15 @@
-import { animation, style, animate } from '@angular/animations';
+import { animation, style, animate, trigger, transition, useAnimation } from '@angular/animations';
 
 export const fadeIn = animation([
   style({ opacity: 0 }),
-  animate('500ms', style({ opacity: 1 }))
+  animate('1000ms', style({ opacity: 1 }))
 ]);
 
 export const fadeOut = animation([
-  animate('500ms', style({ opacity: 0 }))
+  animate('1000ms', style({ opacity: 0 }))
+]);
+
+export const fadeInOut = trigger('fadeInOut', [
+  transition('void => *', useAnimation(fadeIn)),
+  transition('* => void', useAnimation(fadeOut))
 ]);
